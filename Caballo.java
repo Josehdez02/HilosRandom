f3fbe41f4b8d9765beb7445c370ad1e2ae522186
Jose Antonio Mendoza Hernandez <l21020409@veracruz.tecnm.mx>
2023-05-08 23:55:01 -0600

    Clase Caballo iniciada

---
+++ Caballo.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Caballo {
+    private static int distanciaTotal = 100; // La distancia total de la carrera
+    private static int numCaballos;
+    private String nombre;
+    private int distanciaRecorrida;
+    private Random random = new Random();
+    private static char[][] matrizCarrera;
+
+    public Caballo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void run() {
+        System.out.println(nombre + " empieza la carrera!");
+        while (distanciaRecorrida < distanciaTotal) {
+            distanciaRecorrida += random.nextInt(10); // El caballo avanza una distancia aleatoria entre 0 y 9
+            try {
+                Thread.sleep(random.nextInt(1000)); // El caballo espera un tiempo aleatorio entre 0 y 999 milisegundos antes de volver a avanzar
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+
